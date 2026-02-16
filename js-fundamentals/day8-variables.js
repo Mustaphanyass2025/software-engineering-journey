2fc51df0a5a12163000fa6c41bd1cb6e30123f80
Mustapha Nyass <Mustaphanyass928@gmail.com>
2026-02-16 15:55:36 +0000

    Day 8: Introduced JavaScript variables, state creation, state modification, and memory representation using let and const

---
+++ js-fundamentals/day8-variables.js
@@ -0,0 +1,86 @@
+/* Exercise 1 — Create Company State
+
+Write variables for:
+
+company name
+
+company email
+
+company phone
+
+company subscription status
+
+Example structure:
+
+let companyName = "...";
+
+
+Then print them using console.log*/
+
+let companyName = "Ellipseconstruction";
+let companyEmail = "ellipse12@gmail.com";
+let companyPhone = 3140126;
+let companySubscriptionStatus = "active";
+
+console.log(companyName);
+console.log(companyEmail);
+console.log(companyPhone);
+console.log(companySubscriptionStatus);
+
+/* Exercise 2 — Create Project State
+
+Create variables:
+
+project name
+
+project budget
+
+project client name
+
+project status
+
+Print them.*/
+
+let projectName = "Loum Villa";
+let projectBudget = 50000;
+let projectClientName = "Aziz Loum";
+let projectStatus = "Inprogress";
+
+console.log(projectName);
+console.log(projectBudget);
+console.log(projectClientName);
+console.log(projectStatus);
+
+/* Exercise 3 — Modify State
+
+Example:
+
+projectStatus = "completed"
+
+
+Print updated state.
+
+Observe state change.*/
+
+projectStatus = "completed";
+console.log(projectStatus)// updated state
+
+
+/* Exercise 4 — Simulate Staff Assignment
+
+Create variables:
+
+staffName
+staffRole
+assignedProject
+
+
+Print them.*/
+
+let staffName = "Mustapha";
+let staffRole = "admin";
+let assignedProject = "Loum Villa";
+
+console.log(staffName);
+console.log(staffRole);
+console.log(assignedProject);
