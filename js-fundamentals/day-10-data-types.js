bf69acbdaf49a6457556bcb47e98d7a1a217bbf9
Mustapha Nyass <Mustaphanyass928@gmail.com>
2026-02-18 15:45:58 +0000

    Define primitive data type variables for company, project, and staff; check types with typeof

---
+++ js-fundamentals/day-10-data-types.js
@@ -0,0 +1,41 @@
+/* companyName
+companyBudget
+isCompanyActive
+companyOwner
+
+projectName
+projectBudget
+isProjectCompleted
+projectManager (null initially)
+
+staffName
+staffRole
+isStaffActive
+
+typeof companyName
+typeof companyBudget
+etc
+*/
+
+let companyName = "Ellipse Construction";
+let companyBudget = 100000;
+let isCompanyActive = true;
+let companyOwner = "Mr Manga";
+
+let projectName = "Aquaview";
+let projectBudget = 200000;
+let isProjectCompleted = false;
+let projectManager = null;
+
+let staffName = "Mustapha";
+let staffRole = "admin";
+let isStaffActive = true;
+
+console.log(typeof (companyName));
+console.log(typeof (companyBudget));
+console.log(typeof (isCompanyActive));
+console.log(typeof (companyOwner)); console.log(typeof (projectName)); console.log(typeof (projectBudget)); console.log(typeof (isProjectCompleted));
+console.log(typeof (projectManager));
+console.log(typeof (staffName));
+console.log(typeof (staffRole));
+console.log(typeof (isStaffActive));
